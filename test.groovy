fdeab5f6a0b82914f9832ca27cedd8ee3c5683e5
Nic Patterson <arasureynn@gmail.com>
2017-11-25 07:41:33 -0800

    add method that creates stage name

--- test.groovy
+++ test.groovy
@@ -3,4 +3,18 @@ public work(yml, args) {
   println "args :: $args"
 }
 
+/*
+ * Allow the Workflow execution to determine how to name each stage.
+ */
+public getStageName(yml, args, stepName) {
+  switch(stepName) {
+    case 'work':
+      if (args?.name) {
+        return "test : work : ${args.name}"
+      } else {
+        return 'test : work'
+      }
+  }
+}
+
 return this;
